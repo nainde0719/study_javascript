3f13ded30d1793a57aebc8c62515e82d2e7033f9
nainde0719 <nainde0719@gmail.com>
2020-03-23 23:56:35 +0900

    03.23.20

---
+++ 200323/part2/031.js
@@ -0,0 +1,41 @@
+//01
+var obj = {};
+for (var i = 0; i <4; i++) {
+    obj ['key' + i] = i;
+}
+console.log(obj);
+
+var profile = 'chloe:30';
+var person = {
+    [profile] : true,
+    [profile.split(':')[0]]: profile.split(':')[1]
+};
+console.log(person);
+
+//02
+var obj = {};
+for (var i = 0; i < 4; i++) {
+    obj ['key' + i] = i;
+}
+console.log(obj);
+
+var profile = 'chloe:30';
+var person = {
+    [profile] : true,
+    [profile.split(':')[0]]: profile.split(':')[1]
+};
+console.log(person);
+
+//03
+var obj = {};
+for (var i = 0; i < 4; i++) {
+    obj ['key' + i] = i;
+}
+console.log(obj);
+
+var profile = 'chloe:30';
+var person = {
+    [profile] : true,
+    [profile.split(':')[0]]: profile.split(':')[1]
+};
+console.log(person);

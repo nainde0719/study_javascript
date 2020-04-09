07ab07f988f9f304120fa7f9a07cf7c4300d0823
nainde0719 <nainde0719@gmail.com>
2020-04-09 23:21:42 +0900

    04.09.20

---
+++ 200408/109.js
@@ -0,0 +1,37 @@
+const generateRandom = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+for (let i=0; i < 5; i++) {
+    console.log(generateRandom(1, 10));
+}
+
+for (let i=0; i < 5; i++) {
+    console.log(generateRandom(10, 100));
+}
+
+//02
+const generateRandom = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+for (let i=0; i < 5; i++) {
+    console.log(generateRandom(1, 10));
+}
+
+for (let i=0; i < 5; i++) {
+    console.log(generateRandom(10, 100));
+}
+
+//03
+const generateRandom = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+for (let i=0; i < 5; i++) {
+    console.log(generateRandom(1, 10));
+}
+
+for (let i=0; i < 5; i++) {
+    console.log(generateRandom(10, 100));
+}
---
+++ 200408/117.js
@@ -0,0 +1,52 @@
+const jsonStr = '{"drama":"PET","season":2017,"casting":'
+    + '["koyuki","matsumoto jun"],"character":["sumire", "momo"]}';
+
+console.log(JSON.parse(jsonStr));
+console.log(JSON.parse(jsonStr, (key, val) => {
+    if (key === 'season') val = 2003;
+    return val;
+}));
+
+console.log(JSON.parse('13.1'));
+console.log(typeof JSON.parse('13.1'));
+console.log(JSON.parse('false'));
+console.log(typeof JSON.parse('false'));
+
+console.log(JSON.parse('Kiss Carnival'));
+console.log(JSON.parse('[2003, 2017]'));
+
+//02
+const jsonStr = '{"drama":"PET","season":2017,"casting":'
+    + '["koyuki","matsumoto jun"],"character":["sumire", "momo"]}';
+
+console.log(JSON.parse(jsonStr));
+console.log(JSON.parse(jsonStr, (key, val) => {
+    if (key === 'season') val = 2003;
+    return val;
+}));
+
+console.log(JSON.parse('13.1'));
+console.log(typeof JSON.parse('13.1'));
+console.log(JSON.parse('false'));
+console.log(typeof JSON.parse('false'));
+
+console.log(JSON.parse('Kiss Carnival'));
+console.log(JSON.parse('[2003, 2017]'));
+
+//03
+const jsonStr = '{"drama":"PET","season":2017,"casting":'
+    + '["koyuki","matsumoto jun"],"character":["sumire", "momo"]}';
+
+console.log(JSON.parse(jsonStr));
+console.log(JSON.parse(jsonStr, (key, val) => {
+    if (key === 'season') val = 2003;
+    return val;
+}));
+
+console.log(JSON.parse('13.1'));
+console.log(typeof JSON.parse('13.1'));
+console.log(JSON.parse('false'));
+console.log(typeof JSON.parse('false'));
+
+console.log(JSON.parse('Kiss Carnival'));
+console.log(JSON.parse('[2003, 2017]'));
